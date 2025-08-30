e3acef6b815ebef0fcf0dc71ab68aa00f09baea7
NoannNass <92955262+NoannNass@users.noreply.github.com>
2025-08-30 17:30:16 +0200

    all-is-dockeurized-but-cannot-access-partnership-tab

--- user-service/src/test/java/com/app/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/app/userservice/UserServiceApplicationTests.java
@@ -2,9 +2,27 @@ package com.app.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.app.userservice.repository.InterestTagRepository;
+import com.app.userservice.repository.LearningObjectiveRepository;
+import com.app.userservice.repository.SkillTagRepository;
+import com.app.userservice.repository.UserRepository;
 
 @SpringBootTest
 class UserServiceApplicationTests {
+
+    @MockBean
+    private InterestTagRepository interestTagRepository;
+
+    @MockBean
+    private SkillTagRepository skillTagRepository;
+
+    @MockBean
+    private LearningObjectiveRepository learningObjectiveRepository;
+
+    @MockBean
+    private UserRepository userRepository;
 
     @Test
     void contextLoads() {
